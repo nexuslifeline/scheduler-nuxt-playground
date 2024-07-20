e412eb0488f9ea4b132b8d2d0213e56df6728d2d
Paul Christian Rueda <nexuslifeline@gmail.com>
2024-07-20 09:25:45 +0800

    Introduce ViewSelector component

--- app.vue
+++ app.vue
@@ -1,3 +1,23 @@
 <template>
-  <WeekView />
+  <div class="w-full h-screen text-center flex flex-col items-start p-2">
+    <ViewSelector
+      :selectedIndex="selectedIndex"
+      @viewSelected="handleViewSelected"
+    />
+    <div class="flex-grow w-full">
+      <Calendar v-show="selectedIndex === 0" />
+      <WeekView v-show="selectedIndex === 1" />
+      <MonthView v-show="selectedIndex === 2" />
+    </div>
+  </div>
 </template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const selectedIndex = ref(0);
+
+const handleViewSelected = (index: number) => {
+  selectedIndex.value = index;
+};
+</script>
---
+++ components/ViewSelector/Index.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="flex justify-center space-x-4 p-2">
+    <button
+      @click="selectView(0)"
+      :class="buttonClass(0)"
+      class="p-2 border rounded-md text-sm font-medium hover:bg-gray-200"
+    >
+      Default View
+    </button>
+    <button
+      @click="selectView(1)"
+      :class="buttonClass(1)"
+      class="p-2 border rounded-md text-sm font-medium hover:bg-gray-200"
+    >
+      Week View
+    </button>
+    <button
+      @click="selectView(2)"
+      :class="buttonClass(2)"
+      class="p-2 border rounded-md text-sm font-medium hover:bg-gray-200"
+    >
+      Month View
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineEmits, defineProps } from "vue";
+
+const props = defineProps<{
+  selectedIndex: number;
+}>();
+
+const emit = defineEmits<{
+  (e: "viewSelected", index: number): void;
+}>();
+
+const buttonClass = (index: number) => {
+  console.log("props.selectedIndex", props.selectedIndex);
+  return props.selectedIndex === index
+    ? "bg-blue-500 text-white border-blue-500"
+    : "bg-white text-gray-700 border-gray-300";
+};
+
+const selectView = (index: number) => {
+  emit("viewSelected", index);
+};
+</script>
